def0d726f537a8992c7bd5bb4ebee6a735748e4a
hzh <1239649724@qq.com>
2020-10-28 09:08:51 +0800

    add cloud-consumer-hystrix-dashboard9001

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/athuayu/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,18 @@
+package com.athuayu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/*
+ *@author Leelay
+ *@date 2020/10/28 8:52
+ *@Description:
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
