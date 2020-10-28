cbb95af430a73ae2d7fd9142c0764668461499fc
hzh <1239649724@qq.com>
2020-10-28 16:11:11 +0800

    add module cloud-config-client-3355

---
+++ cloud-config-client-3355/src/main/java/com/athuayu/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,20 @@
+package com.athuayu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/*
+ *@author Leelay
+ *@date 2020/10/2815:21
+ *@Description:
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@RefreshScope
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
---
+++ cloud-config-client-3355/src/main/java/com/athuayu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,21 @@
+package com.athuayu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ *@author Leelay
+ *@date 2020/10/28 15:23
+ *@Description:
+ */
+@RestController
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
